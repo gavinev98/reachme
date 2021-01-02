f865abf319c430a2b69b72036f34aa6b4fcf2773
gavinev98 <gavin.everett48@hotmail.com>
2021-01-02 11:36:43 +0000

    configuring the auth file for login and logout.

---
+++ src/services/auth.js
@@ -0,0 +1,31 @@
+import { auth, provider } from "../firebase";
+
+
+//signin with google
+export const signInWithGoogle = async () => {
+    let user;
+
+   await auth.signInWithPopup(provider)
+    .then((res) => {
+        console.log(res.user);
+
+        user = res.user;
+    }).catch((error) => {
+        console.log(error.message);
+    })
+
+    return user;
+}
+
+//logout of account
+export const logoutOfAccount = () => {
+    let logoutSuccess;
+
+    await auth.signOut()
+    .then(() => {
+        logoutSuccess = true;
+    }).catch((error) => {
+        console.log(error.message);
+    })
+
+}
