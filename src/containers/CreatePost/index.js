aab51c8d1ce0c594a8d94739589fe3354d647dc0
gavinev98 <gavin.everett48@hotmail.com>
2021-01-03 11:55:58 +0000

    setting up state for caption of post.

--- src/containers/CreatePost/index.js
+++ src/containers/CreatePost/index.js
@@ -8,12 +8,22 @@ const Index = () => {
     //using react hooks to access the users data.
     const [user, setUser] = useContext(UserContext).user;
 
+    //react hook to store the users caption of post.
+    const [captioon, setCaption] = useState("");
+
+    //react hook to store the users uploaded image.
+
+
 
     return (
         <div className="createpost">
         {user ? 
         <div class="createPostContainer">
          <p className="createPostCaption">Create a Post</p>
+         <div className="loggedIn" >
+         <textarea className="createPostContent" placeholder="Whats on your mind"></textarea>
+
+        </div>
         </div>
         
         
