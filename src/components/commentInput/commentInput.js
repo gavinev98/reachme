507673533bae36a6a639bfc260c67833e3f22eec
gavinev98 <gavin.everett48@hotmail.com>
2021-01-06 15:31:15 +0000

    setting up comment structure.

--- src/components/commentInput/commentInput.js
+++ src/components/commentInput/commentInput.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import "./style.css";
+import { storage, db } from '../../firebase';
 
-const commentInput = () => {
+const commentInput = ({id}) => {
+
+    //database call to add comment to post commented on.
+    const [user, setUser] = useContext(UserContext).user;
+
+    //function to upload comment by user
+    const postCommentHandler = () => {
+
+    }
+
     return (
         <div className="commentInput">
             <textarea placeholder="Write a comment..." rows="1" className="commentInput_TextArea">
 
             </textarea>
-            <button>Post</button>
+            <button className="btnPost">Post</button>
         </div>
     );
 };
--- src/containers/Post/index.js
+++ src/containers/Post/index.js
@@ -56,7 +56,7 @@ const Index = ({profileURL, username, id, photoURL, caption, comments}) => {
            </p>
             </div>
             <p className="commentsCaption"> Comments </p>
-            <CommentInput />
+            <CommentInput id={id} />
 
 
             {comments ? comments.map((singleComment) => 
